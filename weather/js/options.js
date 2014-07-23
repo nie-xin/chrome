0fda0e289809c0b04e9c7bde5aee221342ab1f55
nie-xin <niexin.study@gmail.com>
2014-07-23 23:10:34 +0200

    refactoring

--- weather/js/options.js
+++ weather/js/options.js
@@ -1,6 +1,21 @@
-var city = localStorage.city || 'paris';
-document.getElementById('city').value = city;
-document.getElementById('save').onclick = function() {
-    localStorage.city = document.getElementById('city').value;
-    alert('Save successful');
-}
+var OPTIONS = (function() {
+    var city = localStorage.city || 'paris';
+    var country = localStorage.country || 'france';
+
+    return {
+        init: function() {
+
+            document.getElementById('city').value = city;
+
+            document.getElementById('country').value = country;
+
+            document.getElementById('save').onclick = function() {
+                localStorage.city = document.getElementById('city').value;
+                localStorage.country = document.getElementById('country').value;
+                alert('Saving succeed: ' + localStorage.city + ', ' + localStorage.country);
+            }
+        }
+    };
+})();
+
+OPTIONS.init();
--- weather/js/weather.js
+++ weather/js/weather.js
@@ -1,33 +1,51 @@
-function httpRequest(url, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4) {
-            callback(xhr.responseText);
+var WEATHER = (function() {
+    var city = null,
+        country = null,
+        url = null;
+
+    var init = function() {
+        city = localStorage.city ? localStorage.city : 'paris';
+        country = localStorage.country ? localStorage.country : 'france';
+        url = 'http://api.openweathermap.org/data/2.5/forecast/daily?q='+ city + ',' + country;
+        document.getElementById('location').innerText = city + ', ' + country;
+    };
+
+    var httpRequest = function(url, callback) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", url, true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4) {
+                callback(xhr.responseText);
+            }
         }
-    }
-    xhr.send();
-}
+        xhr.send();
+    };
 
-function showWeather(result) {
-    result = JSON.parse(result);
-    var list = result.list;
-    var table = '<table><tr><th>Date</th><th>Weather</th><th>Min</th><th>Max</th></tr>';
+    var showWeather = function(result) {
+        result = JSON.parse(result);
+        var list = result.list;
+        var table = '<table><tr><th>Date</th><th>Weather</th><th>Min</th><th>Max</th></tr>';
 
-    list.forEach(function(item) {
-        //console.log(item);
-        var d = new Date(item.dt * 1000);
-        table += '<tr>';
-        table += '<td>' + d.getFullYear() + '-' + (d.getMonth()+1) + '-' + d.getDate() + '</td>';
-        table += '<td>' + item.weather[0].description + '</td>';
-        table += '<td>' + Math.round(item.temp.min - 273.15) + ' celsius</td>';
-        table += '<td>' + Math.round(item.temp.max - 273.15) + ' celsius</td>';
-        //console.log(table);
-    });
-    table += '</table>';
-    document.getElementById('weather').innerHTML = table;
-}
+        list.forEach(function(item) {
+            //console.log(item);
+            var d = new Date(item.dt * 1000);
+            table += '<tr>';
+            table += '<td>' + d.getFullYear() + '-' + (d.getMonth()+1) + '-' + d.getDate() + '</td>';
+            table += '<td>' + item.weather[0].description + '</td>';
+            table += '<td>' + Math.round(item.temp.min - 273.15) + ' celsius</td>';
+            table += '<td>' + Math.round(item.temp.max - 273.15) + ' celsius</td>';
+            //console.log(table);
+        });
+        table += '</table>';
+        document.getElementById('weather').innerHTML += table;
+    };
 
-var city = localStorage.city ? localStorage.city : 'paris';
-var url = 'http://api.openweathermap.org/data/2.5/forecast/daily?q='+ city +',france';
-httpRequest(url, showWeather);
+    return {
+        run: function() {
+            init();
+            httpRequest(url, showWeather);
+        }
+    };
+})();
+
+WEATHER.run();
